4a9356f827ae9b4ab06f5ac6bda926dedbde3703
James Paolo Rili <jrili1@my.bcit.ca>
2024-10-24 09:27:49 -0700

    add footer content

--- scripts/loadSkeleton.js
+++ scripts/loadSkeleton.js
@@ -2,9 +2,9 @@
 // (i.e. header and footer)
 const loadSkeleton = () => {
     // add logic as you wish
-   $("nav").load("./components/footer.html")
-   
-   $("footer").load("./components/navbar.html")
-}
+    $("nav").load("./components/footer.html");
+
+    $("footer").load("./components/navbar.html");
+};
 
 loadSkeleton();
