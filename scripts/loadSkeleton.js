8bca0abbb10ac5e23b32b68f4b50564335174ba9
James Paolo Rili <jrili1@my.bcit.ca>
2024-10-24 09:01:38 -0700

    added basic logic

--- scripts/loadSkeleton.js
+++ scripts/loadSkeleton.js
@@ -1,7 +1,10 @@
 // Load reoccuring components in website
 // (i.e. header and footer)
 const loadSkeleton = () => {
-     
+    // add logic as you wish
+   $("nav").load("./components/footer.html")
+   
+   $("footer").load("./components/navbar.html")
 }
 
 loadSkeleton();
