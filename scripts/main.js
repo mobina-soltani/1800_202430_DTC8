64d737c2cd864f924856179249f87d99f3f71b4f
James Paolo Rili <jrili1@my.bcit.ca>
2024-10-24 11:56:40 -0700

    add comments

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ const getRestaurants = () => {
     ];
 };
 
-// modify restaurant data
 const modifyRestaurantCard = (restaurant) => {};
 
 // Load restaurant cards
@@ -16,7 +15,10 @@ const loadRestaurants = () => {
 
     $.get("components/restaurant-card.html", (data) => {
         for (let i = 0; i < restaurants.length; i++) {
+            // add restaurant with id `restaurant-${i}`
             $("#restaurant-list").append($(data).attr("id", `restaurant-${i}`));
+
+            // edit header within
             $(`#restaurant-${i} > div h1`).text(restaurants[i].name);
         }
     });
