2ce1372073a52dde8079048895ffa547f6a5d07a
James Paolo Rili <jrili1@my.bcit.ca>
2024-10-29 16:06:04 -0700

    Add dynamic header and footer to index

--- scripts/loadSkeleton.js
+++ scripts/loadSkeleton.js
@@ -1,12 +1,19 @@
 // Load reoccuring components in website
 // (i.e. header and footer)
 const loadSkeleton = () => {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // If the "user" variable is not null, then someone is logged in
+            // User is signed in.
+            // Do something for the user here.
+            console.log($("header").load("./components/header_after.html"));
+        } else {
+            // No user is signed in.
+            console.log($("header").load("./components/header_before.html"));
+        }
+    });
     // add logic as you wish
-    $("head").append()
-
-    $("header").load("./components/header_after.html");
-
-    $("footer").load("./components/footer.html");
+    console.log($("footer").load("./components/footer.html"));
 };
 
 loadSkeleton();
