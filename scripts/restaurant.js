a022278ce3c18beac7bd8055d10ef7216802e295
James Paolo Rili <jrili1@my.bcit.ca>
2024-11-06 21:44:16 -0800

    display reviewees

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -10,20 +10,66 @@ const getRestaurantInfo = () => {
         .then((restaurantDoc) => {
             thisRestaurant = restaurantDoc.data();
 
-            document.getElementById("restaurantName").innerText = thisRestaurant.name;
-            tags = thisRestaurant.tags
+            document.getElementById("restaurantName").innerText =
+                thisRestaurant.name;
+            tags = thisRestaurant.tags;
             tagsBuffer = "";
             tags.forEach((tag) => {
                 tagsBuffer += `<span class="bg-warning-subtle rounded-2 px-1 py-1 mx-1"> ${tag} </span>`;
             });
             document.getElementById("tags").innerHTML = tagsBuffer;
-            document.getElementById("stars").innerHTML = `${thisRestaurant.stars} ⭐`;
+            document.getElementById(
+                "stars"
+            ).innerHTML = `${thisRestaurant.stars} ⭐`;
         });
 };
 
 // display restaurant reviews
 const getRestaurantReviews = () => {
-    
-}
+    let params = new URL(window.location.href);
+    let restaurantID = params.searchParams.get("restaurantID");
+
+    db.collection("reviews")
+        .where("restaurantID", "==", restaurantID)
+        .get()
+        .then((allReviews) => {
+            var reviews = allReviews.docs;
+
+            // iterate through all the reviews
+            reviews.forEach((doc) => {
+                var review = doc.data();
+                var userID = review.reviewerID;
+
+                // get the user ID
+                // i hate this 
+                db.collection(`users`)
+                    .doc(userID)
+                    .get()
+                    .then((user) => {
+                        let reviewCardTemplate =
+                            document.getElementById("reviewCardTemplate");
+                        let reviewGroup =
+                            document.getElementById("review-list");
+                        var description = review.description;
+                        var stars = review.stars;
+
+                        // modify reviewCard
+                        var reviewCard =
+                            reviewCardTemplate.content.cloneNode(true);
+                        reviewCard.querySelector(
+                            ".reviewDescription"
+                        ).innerText = description;
+                        reviewCard.querySelector(
+                            ".reviewStars"
+                        ).innerText = `${stars} ⭐`;
+
+                        reviewCard.querySelector(".author").innerText =
+                            user.data().name;
+                        reviewGroup.appendChild(reviewCard);
+                    });
+            });
+        });
+};
 
 getRestaurantInfo();
+getRestaurantReviews();
