47483c247130fee5aae66f8e1fb92932189be356
James Paolo Rili <jrili1@my.bcit.ca>
2024-11-10 16:10:17 -0800

    add review id to user profile

--- scripts/review.js
+++ scripts/review.js
@@ -1,17 +1,77 @@
+const params = new URL(window.location.href).searchParams;
+const restaurantID = params.get("restaurantID");
+const reviewID = params.get("userID");
+
 // Select all elements with the class name "star" and store them in the "stars" variable
 var stars = document.querySelectorAll(".star");
+
+const displayRestaurantName = (restaurantID) => {
+    db.collection("places")
+        .doc(restaurantID)
+        .get()
+        .then((doc) => {
+            let restaurant = doc.data();
+            document.getElementById("restaurant-name-here").textContent =
+                restaurant.name;
+        });
+};
 
 // Iterate through each star element
 stars.forEach((star, index) => {
     // Add a click event listener to the current star
     star.addEventListener("click", () => {
         // Fill in clicked star and stars before it
-        for (let i = 1; i <= index+1; i++) {
+        for (let i = 1; i <= index + 1; i++) {
             // Change the text content of stars to 'star' (filled)
             document.getElementById(`star${i}`).textContent = "star";
         }
-        for (let i=index+2; i <= stars.length; i++) {
+        for (let i = index + 2; i <= stars.length; i++) {
             document.getElementById(`star${i}`).textContent = "star_outline";
         }
     });
 });
+
+const writeReview = () => {
+    let rating = 0;
+    stars.forEach((star) => {
+        // Check if the text content of the current 'star' element is equal to the string 'star'
+        if (star.textContent === "star") {
+            rating++;
+        }
+    });
+
+    let description = document.getElementById("description").value;
+    let user = firebase.auth().currentUser;
+    let uid = user !== null ? user.uid : "";
+
+    if (reviewID) {
+        // for editing in the future
+    } else {
+        console.log(rating, restaurantID, uid);
+        db.collection("reviews")
+            .add({
+                restaurantID: restaurantID,
+                reviewerID: uid,
+                stars: rating,
+                description: description,
+            })
+            .then((review) => {
+                // add to reviews on user page
+                db.collection("users").doc(uid).update({
+                    reviews: firebase.firestore.FieldValue.arrayUnion(review.id),
+                });
+            });
+    }
+};
+
+const clearFields = () => {
+    if (window.confirm("Press OK to clear all fields")) {
+        document.getElementById("description").value = "";
+
+        stars.forEach((star) => {
+            star.textContent = "star_outline";
+        });
+    }
+};
+
+displayRestaurantName(restaurantID);
