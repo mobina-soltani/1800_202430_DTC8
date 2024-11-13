989b160593c16926a2cce6d0fbdc417e20d1bd51
James Paolo Rili <jrili1@my.bcit.ca>
2024-11-13 10:45:21 -0800

    add displayName function

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,4 +1,25 @@
+const loadUserDisplayName = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            db.collection("users")
+                .doc(user.uid)
+                .get()
+                .then((userDoc) => {
+                    let nameHere = document.getElementById("name-goes-here");
+                    nameHere.textContent = userDoc.data().name;
+                });
+        }
+    });
+};
 
-const doAll = () => {};
+const loadUserBio = () => {};
 
-doAll();
+const loadUserReviews = () => {};
+
+const doAll = () => {
+    loadUserDisplayName();
+    loadUserBio();
+    loadUserReviews();
+};
+
+window.onload = doAll;
