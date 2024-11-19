55e8cb1a0391186b655751a732f2cd5870beb514
James Paolo Rili <jrili1@my.bcit.ca>
2024-11-18 18:34:20 -0800

    revamp restaurant detail

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -1,29 +1,89 @@
 // display restaurant info
-const getRestaurantInfo = () => {
+const getRestaurantInfo = async () => {
     // get current url
     let params = new URL(window.location.href);
-    let restaurantID = params.searchParams.get("restaurantID");
+    let id = params.searchParams.get("id");
+    const apiKey =
+        "wu6Nl6r_DN60K_OUcqqQqZ46STMVDHJOqWsmTMLBUN0BO4p5hjxro8ragYxkK1vdhwxFzkOGiG8_-DjZ4k3sd0umkkUPyln6CaSmm28jb1aYtMUINogpYCWFoKQzZ3Yx";
 
-    db.collection("places")
-        .doc(restaurantID)
-        .get()
-        .then((restaurantDoc) => {
-            thisRestaurant = restaurantDoc.data();
+    var data;
+    try {
+        const response = await fetch(
+            `https://api.yelp.com/v3/businesses/${id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
 
-            document.getElementById("restaurantName").innerText =
-                thisRestaurant.name;
-            tags = thisRestaurant.tags;
-            tagsBuffer = "";
-            tags.forEach((tag) => {
-                tagsBuffer += `<span class="bg-warning-subtle rounded-2 px-1 py-1 mx-1"> ${tag} </span>`;
-            });
-            document.getElementById("tags").innerHTML = tagsBuffer;
-            document.getElementById(
-                "stars"
-            ).innerHTML = `${thisRestaurant.stars} ⭐`;
-        });
+        data = await response.json();
+    } catch (error) {
+        console.log(error);
+    }
 
-        document.getElementById("review-link").href = `review.html?restaurantID=${restaurantID}`
+    console.log(data);
+    let banner = document.querySelector("#banner");
+    banner.querySelector("#restaurant-img").src = data.image_url;
+    banner.querySelector("#restaurant-name").textContent = data.name;
+    banner.querySelector(
+        "#stars"
+    ).textContent = `${data.rating} ⭐ ${data.review_count}`;
+    banner.querySelector("#redirect-to-yelp").href = data.url;
+
+    banner.querySelector("#toggle-restaurant").addEventListener("click",
+        () => {toggleRestaurant(id)}
+    )
+
+    banner.querySelector("#review-link").href = `review.html?restaurantID=${id}`
+
+    // send name to localStorage to save api call
+    localStorage.setItem("restaurantName", data.name)
+};
+
+// add restaurant to user list
+const addRestaurant = async (userAttributes, alias) => {
+    userAttributes.update({
+        restaurants: firebase.firestore.FieldValue.arrayUnion(alias),
+    });
+    console.log(`added ${alias}`)
+};
+
+// remove restaurant from user list
+const removeRestaurant = async (userAttributes, alias) => {
+    userAttributes.update({
+        restaurants: firebase.firestore.FieldValue.arrayRemove(alias),
+    });
+    console.log(`removed ${alias}`)
+};
+
+// toggle restaurant from user list
+const toggleRestaurant = async (alias) => {
+    let user = await firebase.auth().currentUser;
+    let userAttributes = db.collection("users").doc(user.uid);
+    let userDoc = await userAttributes.get();
+    let userRestaurants = userDoc.data().restaurants;
+
+    let toggleBtn = document.querySelector(`#toggle-restaurant`);
+
+    if (!userRestaurants.includes(alias)) {
+        addRestaurant(userAttributes, alias);
+        // toggle button to "remove from list"
+        toggleBtn.textContent = "Remove from List";
+        toggleBtn.classList.remove("toggle-restaurant");
+        toggleBtn.classList.add("to-remove");
+    } else {
+        removeRestaurant(userAttributes, alias);
+        // toggle button to "add to list"
+        toggleBtn.textContent = "Add to List";
+        toggleBtn.classList.remove("to-remove");
+        toggleBtn.classList.add("toggle-restaurant");
+    }
+
+    userDoc = await userAttributes.get();
+    userRestaurants = userDoc.data().restaurants;
+    console.log(userRestaurants);
 };
 
 // display restaurant reviews
@@ -43,7 +103,7 @@ const getRestaurantReviews = () => {
                 var userID = review.reviewerID;
 
                 // get the user ID
-                // i hate this 
+                // i hate this
                 db.collection(`users`)
                     .doc(userID)
                     .get()
