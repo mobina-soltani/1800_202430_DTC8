50af69da1ba3c3a76ed1afb4a9a8bc87572437e0
James Paolo Rili <jrili1@my.bcit.ca>
2024-11-25 20:36:41 -0800

    implement delete review

--- scripts/profile.js
+++ scripts/profile.js
@@ -6,28 +6,49 @@ const loadUserDisplayName = async (userID) => {
 
 const loadUserBio = () => {};
 
+const getRestaurantInfo = async (id) => {
+    // get current url
+    const apiKey =
+        "wu6Nl6r_DN60K_OUcqqQqZ46STMVDHJOqWsmTMLBUN0BO4p5hjxro8ragYxkK1vdhwxFzkOGiG8_-DjZ4k3sd0umkkUPyln6CaSmm28jb1aYtMUINogpYCWFoKQzZ3Yx";
+
+    var data;
+    try {
+        const response = await fetch(
+            `https://api.yelp.com/v3/businesses/${id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        data = await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+
+    return data;
+};
+
 const loadRestaurants = async (userID) => {
     let userDoc = await db.collection("users").doc(userID).get();
     let restaurantIDs = userDoc.data().restaurants;
     let template = document.getElementById("restaurant-card");
     let restaurantList = document.getElementById("restaurant-list");
 
-    restaurantIDs.forEach((restaurantID) => {
+    restaurantIDs.forEach(async (restaurantID) => {
         let card = template.content.cloneNode(true);
-        db.collection("places")
-            .doc(restaurantID)
-            .get()
-            .then((restaurant) => {
-                let thisRestaurant = restaurant.data();
-                card.querySelector(".restaurant-name").textContent =
-                    thisRestaurant.name;
-                let restaurantCardElement = card.querySelector(".tags");
-                thisRestaurant.tags.forEach((tag) => {
-                    restaurantCardElement.innerHTML += `<span class="tag">${tag}</span>`;
-                });
+        let restaurantInfo = await getRestaurantInfo(restaurantID)
 
-                restaurantList.appendChild(card);
-            });
+        card.querySelector(".restaurant-name").textContent = restaurantInfo.name
+        card.querySelector(".restaurant-rating").textContent = `${restaurantInfo.rating} ⭐`
+
+        card.querySelector("button").addEventListener("click", () => {
+            window.location.href = `../restaurant.html?id=${restaurantID}`
+        })
+
+        restaurantList.appendChild(card);
     });
 };
 
@@ -38,20 +59,30 @@ const loadUserReviews = async (userID) => {
     let reviewList = document.getElementById("review-list");
 
     reviewIDs.forEach(async (reviewID) => {
-        let card = template.content.cloneNode(true);
-        
         let reviewDoc = await db.collection("reviews").doc(reviewID).get()
-        let review = reviewDoc.data();
-        let restaurantDoc = await db.collection("places").doc(review.restaurantID).get();
-        let restaurant = restaurantDoc.data();
+        let reviewInfo = reviewDoc.data()
+        let card = template.content.cloneNode(true);
 
-        card.querySelector(".restaurant-name").textContent = restaurant.name;
-        card.querySelector(".number-of-stars").textContent = `${review.stars} ⭐`;
-        card.querySelector(".description").textContent = review.description;
+        card.querySelector(".restaurant-name").textContent = `${reviewInfo.restaurantName} (${reviewInfo.stars} ⭐)`
+        card.querySelector(".description").textContent = reviewInfo.description 
+
+        card.querySelector("#remove-review").addEventListener("click", () => {
+            card.className = "";
+            deleteReview(reviewID);
+        })
 
         reviewList.appendChild(card)
     });
 };
+
+const deleteReview = async (reviewID) => {
+    try {
+        await db.collections("reviews").doc(reviewID).delete()
+        console.log(`${reviewID} deleted!`);
+    } catch(e) {
+        console.log(error);
+    }
+}
 
 const doAll = async () => {
     firebase.auth().onAuthStateChanged((user) => {
